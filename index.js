cebc2be5ac2d31ac499d1027c665cd3737ff7bcc
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2021-07-01 15:03:21 -0400

    Add save function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 let countEl= document.getElementById("count-el");
-let saveEl = docume.getElementById("save-el");
+let saveEl = document.getElementById("save-el");
 
 let count = 0; 
 
@@ -8,4 +8,9 @@ function increment() {
     count += 1;
     countEl.textContent = count;
     return;
+}
+
+function save() {
+    let countstr = count + " - "; 
+    saveEL.textContent += countstr;
 }
