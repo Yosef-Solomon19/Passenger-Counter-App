e200c6c4341a21162b74f95c7c43aaadb1d75c3a
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2022-04-13 16:50:26 -0400

    starting increment function for increment button.

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let count = 0; 
+
+// Store count as 0 
+// listen for clicks on the inc button
+// increment the count variable when the butoon is clicked
+// change the count-el in the HTML to reflect the new count
+
+function increment() {
+    count++;
+    console.log(count); 
+}
