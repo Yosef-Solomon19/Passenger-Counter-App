bd0cd3b7f045c7399f398b239979e9fd0f9eefe4
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2022-04-19 14:51:47 -0400

    Modified save function to also reset count.

--- index.js
+++ index.js
@@ -7,14 +7,14 @@
 // increment the count variable when the butoon is clicked
 // change the count-el in the HTML to reflect the new count
 let count = 0; 
-let countEL= document.getElementById("count-el");
+let countEl= document.getElementById("count-el");
 
 // When the increment button is clicked 
 // It will call the function to add the count by 1
 // and modify the html element to reflect the current counrt.
 function increment() {
     count++; 
-    countEL.innerText= count; 
+    countEl.innerText= count; 
     console.log(count); 
 }
 
@@ -22,9 +22,22 @@ function increment() {
 // Currently all it does is logs out the current count.
 // 15/4/2022 Issue - No space for the first entry. Can't add space since 
 // it'll add space for each entry. Only want space for the first entry.
-let entryLog = document.getElementById("entry-log");
-function save() {
-    entryLog.innerText += `${count}-`;
-    console.log(count);
+// let entryLog = document.getElementById("entry-log");
+// function save() {
+//     entryLog.innerText += `${count}-`;
+//     console.log(count);
+// }
 
+// 19/4/2022 // 
+// Creating Save Feature 
+// .innerText returns the text content of an element without CSS hidden text spacing and tags
+// .textContent returns content of the element with spacing and CSS hiddent text. 
+let saveEl = document.getElementById("save-el");
+function save() {
+    saveEl.textContent += ` ${count} -`;
+    console.log(count);
+    //Setting the count to 0 Everytime we hit save.
+    count = 0; 
+    countEl.innerText = count;
 }
+
