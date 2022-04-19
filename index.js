13203059aca544b66f479cea14e91c3fe35fedf1
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2022-04-19 15:54:56 -0400

    Added decrement function. Will attempt to work on a function to save the entries that were made.

--- index.js
+++ index.js
@@ -40,4 +40,30 @@ function save() {
     count = 0; 
     countEl.innerText = count;
 }
+// When the decrement button is clicked 
+// It will call the function to subtract the count by 1
+// and modify the html element to reflect the current count. 
+// potential issue: Can you have negative numbers ?
+//                  How to prevent getting negative numbers 
 
+function decrement() {
+    count--;
+    if (count < 0) {
+        count = 0; 
+    } else {
+        countEl.innerText = count;
+        console.log(count)
+    }
+    // countEl.innerText = count;
+    // console.log(count)
+}
+
+// Save Current Entry 
+// when the function is called it will 
+// Save the entries made with date and time included for the day it was saved.
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement#examples
+// https://stackoverflow.com/questions/10211145/getting-current-date-and-time-in-javascript
+// const currentDate = new Date().toLocaleDateString(
+//     'en-GB', {hour: "numeric",
+//               minute: "numeric"});
+// console.log(currentDate);
